9eae9ff42820282921285af092ee735307004637
alcbeatriz <beatriz.cristina@ufvjm.edu.br>
2020-10-05 21:29:47 -0300

    Update Atv9.py

--- Revisao02 - IA/Atv9.py
+++ Revisao02 - IA/Atv9.py
@@ -1,36 +1,28 @@
-#Faça um programa que leia 10 tarefas. Cada tarefa contém uma descrição (string) e a ordem que
+#Faça um programa que leia 10 tarefas. Cada tarefa contém uma descricaoricaoricaorição (string) e a ordem que
 #ela deve ser executada (utilizar classe ou estrutura para representar a tarefa). As tarefas devem ser
-#inseridas em um vetor. O programa deve imprimir a descrição tarefas em ordem de execução.
+#inseridas em um vetor. O programa deve imprimir a descricaoricaoricaorição tarefas em ordem de execução.
+import heapq
 
-fila = []
-print("Fila: ", fila)
+class Tarefas:
+    def _init_ (self,descricao,prioridade):
+        self.descricao = descricao
+        self.prioridade = prioridade
 
-fila.append("Tarefa E")
-print("Inserindo um elemento no final da fila: ", fila)
+    def getDescricao(self):
+        return self.__descricao
 
-fila.append("Tarefa I")
-print("Inserindo outro elemento no final da fila: ", fila)
+    def setDescricao(self, descricao):
+        self.__descricao = descricao
 
-fila.append("Tarefa C")
-print("Inserindo outro elemento no final da fila: ", fila)
+lista=[]
+for i in range (0,10):
+    x=input('Digite a descrição da tarefa {}: '.format(i+1))
+    y=int(input('Digite a prioridade da tarefa {}: '.format(i+1)))
+    while y>5 or y<0:
+       y=int(input('PRIORIDADE INVALIDA: DIGITE UM VALOR DE 0 A 5 {}: '.format(i+1)))
+    l=(y, x)
+    heapq.heappush(lista, l)
 
-fila.append("Tarefa F")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa B")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa H")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa J")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa A")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa D")
-print("Inserindo outro elemento no final da fila: ", fila)
-
-fila.append("Tarefa G")
-print("Inserindo outro elemento no final da fila: ", fila)
+for i in range (0, 10):
+    print(lista[-i][1])
+    heapq.heappop(lista)
